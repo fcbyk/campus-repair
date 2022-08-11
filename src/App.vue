15e16fc0d6f972e4d953fcb6f05ae0f70df6ceca
Bykit <731240932@qq.com>
2022-08-11 20:44:57 +0800

    login-1

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,66 @@
 <template>
   <div id="app">
+
     <div class="change-icon">
       <i class="el-icon-setting"></i>
     </div>
     <p>热海大报修系统</p>
+
     <div class="formm">
       <el-input v-model="id" placeholder="请输入账号" id="idd"></el-input>
       <el-input placeholder="请输入密码" v-model="pw" show-password></el-input>
       <div id="wm"><a href="javascript:void">忘记密码?&nbsp;&nbsp;</a></div>
       <button class="btn-login" >登录</button>
     </div>
+
     <div class="bott">
-      <a href="javascript:void">注册账号</a>
+      <a @click="dialogVisible = true">注册账号</a>
       <a @click="drawer = true" @mouseup="pwDrawer">测试账号</a>
       <a href="https://www.yuque.com/docs/share/7e6c1b5d-5487-4c0f-8384-1ea451914d51?# 《课程设计报告》" target="blank">课程设计报告</a>
       <a href="https://element.eleme.cn/#/zh-CN" target="blank">Element UI</a>
     </div>
+
     <el-drawer
       title="已注册账号"
       :visible.sync="drawer"
       direction="ltr"
       :before-close="handleClose">
-      
-      <p>{{ax}}</p>
+      <div v-for="p in ax" :key="p.id" class="yzc">
+        <p>账号:{{p.id}}</p>
+        <p>密码:{{p.pw}}</p>
+      </div>
     </el-drawer>
+
+    <el-dialog
+      title="注册账号"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="cancel">
+
+      <el-form ref="form" :model="form" label-width="80px">
+          <el-form-item label="我是">
+            <el-select v-model="form.region" placeholder="你的身份">
+              <el-option label="学生" value="shanghai"></el-option>
+              <el-option label="教职工" value="beijing"></el-option>
+              <el-option label="维修师傅" value="beijinghh"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="设置账号">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="设置密码">
+            <el-input v-model="form.zpw1" show-password></el-input>
+          </el-form-item>
+          <el-form-item label="确认密码">
+            <el-input v-model="form.zpw2" show-password></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">立即创建</el-button>
+            <el-button @click="cancel">取消</el-button>
+          </el-form-item>
+      </el-form>
+    </el-dialog>
+
   </div>
 </template>
 
@@ -42,7 +79,14 @@ export default {
       pw:'',
       radio:'',
       drawer: false,
-      ax:'我来了！123'
+      ax:'我来了！123',
+      dialogVisible: false,
+      form: {
+          name: '',
+          region: '',
+          zpw1:'',
+          zpw2:'',
+      }
     }
   },
   methods: {
@@ -55,11 +99,25 @@ export default {
     },
     pwDrawer(){
         // drawer = true
-        console.log('123')
+        // console.log('123')
         axios.get('/').then(res=>{
-          console.log(this)
+          // console.log(this)
           this.ax = res.data
         })
+    },
+    cancel(){
+        this.$confirm('确认关闭？')
+        .then( () => {
+          this.dialogVisible=false
+          this.form.name= ''
+          this.form.region= ''
+          this.form.zpw1=''
+          this.form.zpw2=''
+        })
+        .catch( ()=> {});
+    },
+    onSubmit() {
+      console.log('submit!');
     }
   }
 }
@@ -138,5 +196,11 @@ export default {
 .bott a:hover{
   color: rgb(55, 55, 55);
   text-decoration-line: underline;
+}
+.yzc{
+  margin: 20px;
+  border: #a9a9a9 solid 1px;
+  padding: 10px;
+  border-radius: 10px;
 }
 </style>
