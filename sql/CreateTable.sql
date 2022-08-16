2469e48965b07b48ac53e16c13a9da6300a7c29f
Bykit <731240932@qq.com>
2022-08-16 16:01:41 +0800

    sql

--- sql/CreateTable.sql
+++ sql/CreateTable.sql
@@ -7,12 +7,20 @@ show create database hntou_repair;
 show variables like 'character_%';
 drop database hntou_repair;
 
+create table user_information(
+	user_id varchar(20) primary key,
+    user_name varchar(20) default '新用户',
+    user_gender char check(user_gender in('男','女')),
+    user_phone varchar(20) default '未绑定',
+    user_sort varchar(10) check(user_sort in('student','staff','repairMan')) not null
+);
+
 create table register(
     id varchar(20) primary key,
     pw varchar(20) not null,
     sort varchar(10) check(sort in('student','staff','repairMan'))
 );
-drop table repairMan;
+drop table user_information;
 show create table register;
 
 insert into register values('s123','123456','student');
@@ -63,18 +71,31 @@ create trigger new_register
 after insert on register 
 for each row
 begin
-if(LEFT(new.id,1) ='s')
-then insert into student(sid) value(new.id);
-end if;
-if(LEFT(new.id,1) ='t')
-then insert into staff(tid) value(new.id);
-end if;
-if(LEFT(new.id,1) ='r')
-then insert into repairMan(rid) value(new.id);
-end if;
+insert into user_information(user_id,user_sort) value(new.id,new.sort);
 end //
 DELIMITER ;
 
 SHOW TRIGGERS;
-
 DROP TRIGGER new_register;
+
+create view simple_account as 
+select register.id, register.pw, user_information.user_name as name
+from register,user_information 
+where register.id = user_information.user_id;
+
+select * from simple_account;
+drop view student_information;
+
+create view student_information as
+select user_id as sid, user_name as sname, user_gender as gender,user_phone as phone
+from user_information where user_sort='student';
+
+create view staff_information as
+select user_id as tid, user_name as tname, user_gender as gender,user_phone as phone
+from user_information where user_sort='staff';
+
+create view repairMan_information as
+select user_id as rid, user_name as rname, user_gender as gender,user_phone as phone
+from user_information where user_sort='repairMan';
+
+select * from student_information;
